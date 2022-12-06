d4f89af6413c62114bd5fa4995760f9c480b2b34
Yordan Tarazona <yordantarazona9@gmail.com>
2022-12-05 20:52:02 -0500

    Se agrego el log

--- src/app.py
+++ src/app.py
@@ -1,45 +1,50 @@
 # Importing the libraries that are needed for the program to run.
-from flask import Flask, jsonify, request, Response
+from flask import Flask, jsonify, request, Response, current_app
 from flask_pymongo import PyMongo
 from bson import json_util
 from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
+import logging
 from bson.objectid import ObjectId
-
 
 # Creating a Flask application instance.
 app = Flask(__name__)
-
 
 # Setting the secret key for the Flask app and the MongoDB URI.
 app.secret_key = generate_password_hash("APIMongoPythonFlaskMonitoreo")
 app.config["MONGO_URI"] = "mongodb://localhost:27017/invernadero"
 
-
 # Creating a connection to the MongoDB database.
 mongo = PyMongo(app)
 
+time = str(datetime.today().strftime('%B %d %Y'))
+salida = str(f'../logs/salida_({str(time)}).log')
+logging.basicConfig(filename=salida, level=logging.DEBUG)
 
 """
     It takes a JSON object, inserts it into a MongoDB collection, and returns the ID of the inserted
     object.
     :return: The response is a JSON object with the _id of the inserted document.
 """
+
+
 @app.route('/api/v1/addDatos', methods=['POST'])
 def post_addDatos():
-    print(request.json)
+    app.logger.info(f'Funcionalidad post_addDatos... {datetime.now()}')
     datos = request.json
     if datos:
         id = mongo.db.datoCollection.insert_many(
             datos
         )
-        print("estos son los datos --> ",str(id.inserted_ids))
+        # print("estos son los datos --> ",str(id.inserted_ids))
         response = jsonify({
             '_id': str(id.inserted_ids),
         })
         response.status_code = 201
+        app.logger.info(f'Cantidad de datos -> {str(id.inserted_ids)} <--> {datetime.now()}')
         return response
     else:
+        app.logger.error(f' addDatos incorrectos <--> {datetime.now()}')
         return {'message': 'addDatos incorrectos'}
 
 
@@ -47,10 +52,14 @@ def post_addDatos():
     It takes a request, queries the database, and returns a response
     :return: A list of dictionaries.
 """
+
+
 @app.route('/api/v1/datos', methods=['GET'])
 def get_datos():
+    app.logger.info(f'Funcionalidad get_datos...{datetime.now()}')
     datos = mongo.db.datoCollection.find()
     response = json_util.dumps(datos)
+    app.logger.info(f'Cantidad de datos -> {response.count("_id")} <--> {datetime.now()}')
     return Response(response, mimetype="application/json")
 
 
@@ -68,15 +77,19 @@ def get_datos():
         ...
     ]
 """
+
+
 @app.route('/api/v1/filterDatos', methods=['POST'])
 def post_filterDatos():
+    app.logger.info(f'Funcionalidad post_filterDatos...{datetime.now()}')
     fecha_ini = request.json["fecha_ini"]
     fecha_fin = request.json["fecha_fin"]
-    diccionario = {"FECHA": {"$gte": str(datetime.strptime(fecha_ini, "%Y-%m-%d")), "$lte": str(datetime.strptime(fecha_fin, "%Y-%m-%d"))}}
-    print(diccionario)
+    diccionario = {"FECHA": {"$gte": str(datetime.strptime(fecha_ini, "%Y-%m-%d")),
+                             "$lte": str(datetime.strptime(fecha_fin, "%Y-%m-%d"))}}
+    app.logger.info(f'Consulta {diccionario} <--> {datetime.now()}')
     registros = mongo.db.datoCollection.find(diccionario)
     response = json_util.dumps(registros)
-    #return jsonify(registros)
+    # return jsonify(registros)
     return Response(response, mimetype="application/json")
 
 
@@ -87,13 +100,16 @@ def post_filterDatos():
     :param fecha_fin: 2020-01-01
     :return: The response is a JSON object with the data that is in the database.
 """
+
+
 @app.route('/api/v1/filterDatos/<fecha_ini>&<fecha_fin>', methods=['GET'])
 def get_filterDatos(fecha_ini, fecha_fin):
-    diccionario = {"FECHA": {"$gte": str(datetime.strptime(fecha_ini, "%Y-%m-%d")), "$lte": str(datetime.strptime(fecha_fin, "%Y-%m-%d"))}}
-    print(diccionario)
+    app.logger.info(f'Funcionalidad get_filterDatos...{datetime.now()}')
+    diccionario = {"FECHA": {"$gte": str(datetime.strptime(fecha_ini, "%Y-%m-%d")),
+                             "$lte": str(datetime.strptime(fecha_fin, "%Y-%m-%d"))}}
+    app.logger.info(f'Consulta {diccionario} <--> {datetime.now()}')
     registros = mongo.db.datoCollection.find(diccionario)
     response = json_util.dumps(registros)
-    response
     return Response(response, mimetype="application/json")
 
 
@@ -101,12 +117,16 @@ def get_filterDatos(fecha_ini, fecha_fin):
     It returns a JSON object with a message key and a value of "API YA ESTA ARRIBA INVERNADERO"
     :return: A dictionary with a key and value.
 """
+
+
 @app.route('/', methods=['GET'])
 def main():
+    app.logger.info(f'Estado de API...{datetime.now()}')
     return {'message': 'API YA ESTA ARRIBA INVERNADERO'}
 
 
 # Running the Flask app.
 if __name__ == ('__main__'):
+    app.logger.info(f'Se Inicia el Script de la API...{datetime.now()}')
     main()
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="192.168.2.112", port=5000, debug=True)
